e400a4ddf0489ea6b04529349a6e2adf594502d4
K0veKvaS <vovanco03@mail.ru>
2024-11-01 20:09:18 +0300

    Lab2.2 Created main.rb

---
+++ Core classes and models/class_name.rb
@@ -0,0 +1,7 @@
+require_relative   'class.rb'
+
+student1 = Student.new("Олег", "Воевода", "", 1, "+78474232345", nil, "randommail@mail.ru", nil)
+student2 = Student.new("Андрей", "Христофоров", "Баламутович", 10, "+77543752543","shyguy", nil, "Gite")
+
+puts student1
+puts student2
