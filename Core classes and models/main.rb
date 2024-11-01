48c60d9a43115383e502119ad7e70393544cc606
K0veKvaS <vovanco03@mail.ru>
2024-11-01 21:10:39 +0300

    Lab2.2 Updated main

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -1,7 +1,7 @@
-require_relative   'class.rb'
+require_relative 'student'
 
-student1 = Student.new("Олег", "Воевода", "", 1, "+78474232345", nil, "randommail@mail.ru", nil)
-student2 = Student.new("Андрей", "Христофоров", "Баламутович", 10, "+77543752543","shyguy", nil, "Gite")
+student1 = Student.new(first_name: "Олег", second_name: "Воевода", third_name: "", id: 1, telephone: "+78474232345", telegram: nil, email: "randommail@mail.ru", git: nil)
+student2 = Student.new(first_name: "Андрей", second_name: "Христофоров", third_name: "Баламутович", id: 10, telephone: "+77543752543", telegram: "shyguy", email: nil, git: "Gite")
 
-puts student1
-puts student2
+puts student1.info
+puts student2.info
