8cf2e05e8ba4b0693215fe1d3a2ff3cfc9ae8d8d
K0veKvaS <vovanco03@mail.ru>
2024-11-08 20:17:22 +0300

    Lab2.3 added conditions "contacts" in main

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -1,7 +1,13 @@
-require_relative 'student'
+require_relative 's1'
 
-student1 = Student.new(first_name: "Олег", second_name: "Воевода", third_name: "", id: 1, telephone: "+78474232345", telegram: nil, email: "randommail@mail.ru", git: nil)
-student2 = Student.new(first_name: "Андрей", second_name: "Христофоров", third_name: "Баламутович", id: 10, telephone: "+77543752543", telegram: "shyguy", email: nil, git: "Gite")
-
+student1 = Student.new(first_name: "Олег", second_name: "Воевода", third_name: "Олегович", id: 1, git: "Gite")
+student2 = Student.new(first_name: "Андрей", second_name: "Христофоров", third_name: "Баламутович", id: 10, git: "")
+student1.set_contacts(telephone: "+77543752543", telegram: "@shyguy", email: "imgonna@no.ru")
+if student1.validate()
+  puts"\n"
+  puts"у студента #{student1.first_name()} #{student1.second_name()} есть контакты(т.е. почта, телега и почта)"
+else
+  puts"у студента #{student1.first_name()} #{student1.second_name()} ничего нет."
+end
 puts student1.info
 puts student2.info
