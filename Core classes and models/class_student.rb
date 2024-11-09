74a80e0459edd5cf1b99ad1a887f7a11eeb545c8
K0veKvaS <vovanco03@mail.ru>
2024-11-09 09:36:40 +0300

    Lab2.9 class connections

--- Core classes and models/class_student.rb
+++ Core classes and models/class_student.rb
@@ -1,4 +1,6 @@
-class Student
+require_relative 'Person'
+
+class Student < Person
   attr_reader :id, :first_name, :second_name, :third_name, :telephone, :telegram, :email, :git
   def initialize(first_name:, second_name:, third_name:, id: nil, git:)
     self.first_name = first_name 
@@ -10,11 +12,32 @@ class Student
     def set_contacts(telephone: nil, telegram: nil, email: nil)
     self.telephone=(telephone) 
     self.telegram=(telegram) 
-    self.email=(email) 
+    self.email=(email)
+    @contact = set_contact(telephone, telegram, email) 
   end
 
   def info 
-    "ID: #{@id || 'Не указано'}, ФИО: #{@first_name} #{@second_name} #{@third_name}, Телефон: #{@telephone}, Telegram: #{@telegram}, Email: #{@email}, Git: #{@git}"
+    "ID: #{@id || 'Не указано'}, ФИО: #{@first_name} #{@second_name} #{@third_name}, Контакты: #{@contact}, Git: #{@git}"
+  end
+
+  def getInfo
+    "#{surname_initials}; Git: #{@git}; Contact: #{contact_choose}"
+  end
+
+  def surname_initials
+    "#{@second_name} #{@first_name[0]}.#{@third_name[0]}."
+  end
+
+  def contact_choose
+    if @telephone
+      "telephone: #{@telephone}"
+    elsif @telegram
+      "Telegram: #{@telegram}"
+    elsif @email
+      "Email: #{@email}"
+    else
+      "Ни один из контактов не указан."
+    end
   end
   
   def validate
