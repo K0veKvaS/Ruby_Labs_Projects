e205f662f623a78973606ea350a3f545caa1d6de
K0veKvaS <vovanco03@mail.ru>
2024-11-22 20:56:47 +0300

    Lab 2.12 refactoring

--- Core classes and models/student_short.rb
+++ Core classes and models/student_short.rb
@@ -1,48 +1,15 @@
-require_relative 'Person'
+require_relative 'person'
 
-class Student_short < Person
-  attr_reader :id, :surname_initials, :git, :contact
+class StudentShort < Person
+  attr_reader :id, :git, :contact
 
-  def initialize(student)
-    @id = student.id
-    @surname_initials = student.surname_initials
-    @git = student.git
-    @contact = student.contact_choose
-  end
+  def initialize(id:, student: nil)
+    if student.is_a?(Student)
+      @id = student.id 
+      super(second_name: student.second_name, first_name: student.first_name, last_name: student.last_name)
+      @git = student.git
+      @contact = student.send(:contact_method)
 
-  def initialize_from_info_string(id, info_string)
-    details = parse_info_string(info_string)
-    @id = id
-    @surname_initials = "#{details[:second_name]} #{details[:first_name][0]}.#{details[:third_name][0]}."
-    @git = details[:git]
-    @contact = contact_choose(details)
-  end
-
-  def getInfo
-    "#{@surname_initials}; Git: #{@git}; Contact: #{@contact}"
-  end
-
-  private
-
-  def parse_info_string(info_string)
-    details = info_string.split(';').map(&:strip)
-    {
-      first_name: details[0],
-      second_name: details[1],
-      third_name: details[2],
-      git: details[3]
-    }
-  end
-
-  def contact_choose(details)
-    if details[:telephone]
-      "telephone: #{details[:telephone]}"
-    elsif details[:telegram]
-      "Telegram: #{details[:telegram]}"
-    elsif details[:email]
-      "Email: #{details[:email]}"
-    else
-      "Ни один из контактов не указан."
     end
   end
 end
