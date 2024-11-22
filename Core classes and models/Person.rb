3f3e83018a6f9ab8bd8a33dae82ddcefc92891dc
K0veKvaS <vovanco03@mail.ru>
2024-11-22 20:57:20 +0300

    Lab 2.3 refactoring

--- Core classes and models/Person.rb
+++ Core classes and models/Person.rb
@@ -1,51 +1,38 @@
 class Person
-  attr_reader :surname_initials, :git, :contact, :telephone, :telegram, :email
+  attr_accessor :second_name, :first_name, :last_name
 
-  def initialize(second_name:, first_name:, third_name:, git:, telephone: nil, telegram: nil, email: nil)
-    @surname_initials = "#{second_name} #{first_name[0]}.#{third_name[0]}."
-    self.git = git
-    set_contacts(telephone: telephone, telegram: telegram, email: email)
+  def initialize(second_name:, first_name:, last_name: nil)
+    self.second_name = second_name
+    self.first_name = first_name
+    self.last_name = last_name
   end
 
-  def contact_choose
-    if telephone
-      "telephone: #{telephone}"
-    elsif telegram
-      "Telegram: #{telegram}"
-    elsif email
-      "Email: #{email}"
-    else
-      "Ни один из контактов не указан."
-    end
+  def first_name=(first_name)
+    @first_name = first_name.to_s =~ /^[A-Za-zА-Яа-яЁё'-]+$/ ? first_name : nil
   end
 
-  def info 
-    "ФИО: #{surname_initials}, Контакты: #{@contact}, Git: #{@git}"
+  def second_name=(second_name)
+    @second_name = second_name.to_s =~ /^[A-Za-zА-Яа-яЁё'-]+$/ ? second_name : nil
   end
 
-  def set_contacts(telephone: nil, telegram: nil, email: nil)
-    self.telephone = telephone
-    self.telegram = telegram
-    self.email = email
-    @contact = contact_choose(telephone, telegram, email)
+  def last_name=(last_name)
+    @last_name = last_name.to_s =~ /^[A-Za-zА-Яа-яЁё'-]+$/ ? last_name : nil
+  end
+
+  def initials
+    "#{@second_name} #{initials_string}"
   end
 
   private
-
-  def self.telephone?(telephone)
-    @telephone = telephone.to_s =~ /^\+\d{11}$/ ? telephone : nil
+  def initials_string
+    "#{@first_name[0]}.#{@last_name ? " #{@last_name[0]}" : ''}."
   end
 
-  def telegram=(telegram)
-    @telegram = telegram =~ /^@[a-zA-Z0-9_]{5,32}$/ ? telegram : nil
-  end
-
-  def email=(email)
-    @email = email =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]{2,})\z/ ? email : nil
-  end
-
-  def git=(git)
-    @git = git =~ /^[a-zA-Z0-9_.+-]+$/ ? git : nil
+  protected
+  def contact_method
+    return "Телефон: #{@telephone}" if @telephone
+    return "Telegram: #{@telegram}" if @telegram
+    return "Email: #{@email}" if @email
+    "Контактная информация недоступна"
   end
 end
-
