abe3b995b0e7a574d80dc275f3011c218a77582a
K0veKvaS <vovanco03@mail.ru>
2024-12-07 00:04:01 +0300

    Lab2.13 added parsing

--- Core classes and models/student_short.rb
+++ Core classes and models/student_short.rb
@@ -1,15 +1,32 @@
-require_relative 'person'
+require_relative 'student'
 
-class StudentShort < Person
-  attr_reader :id, :git, :contact
+class StudentShort < Student
+  attr_reader :git, :id, :initials, :contact
 
-  def initialize(id:, student: nil)
+  def initialize(student: nil, id: nil, info_string: nil)
     if student.is_a?(Student)
-      @id = student.id 
-      super(second_name: student.second_name, first_name: student.first_name, last_name: student.last_name)
+      super(second_name: student.second_name, first_name: student.first_name, last_name: student.last_name,
+            telephone: student.telephone, telegram: student.telegram, email: student.email, git: student.git)
       @git = student.git
-      @contact = student.send(:contact_method)
-
+      @id = student.id
+      @initials = student.get_initials 
+      @contact = student.contact_method 
+    elsif info_string
+      parse_info_string(info_string)
+      @id = id
+      @contact = "Контактная информация недоступна" 
     end
+  end
+
+  # Метод для парсинга строки информации
+  def parse_info_string(info_string)
+    parts = info_string.split(' ')
+    self.second_name = parts[0]
+    self.first_name = parts[1]
+    self.last_name = parts[2] if parts.length > 2
+  end
+
+  def getInfo
+    "ID: #{@id}, Фамилия и инициалы: #{initials}, Git: #{@git}, контакты: #{@contact}"
   end
 end
