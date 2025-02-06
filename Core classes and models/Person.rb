de2f3f373c146934cbf29d5fb01107254254f1eb
K0veKvaS <vovanco03@mail.ru>
2025-02-06 21:35:28 +0300

    Lab 2.7 redo and expanded Person

--- Core classes and models/Person.rb
+++ Core classes and models/Person.rb
@@ -1,31 +1,56 @@
 class Person
-  attr_reader :second_name, :first_name, :last_name, :id, :git
+  attr_reader :id, :first_name, :second_name, :third_name
 
-  def initialize(second_name:, first_name:, last_name: nil, id: nil, git: nil)
-    self.second_name = second_name
-    self.first_name = first_name
-    self.last_name = last_name
-    self.id = id
-    self.git = git
+  def initialize(attributes = {id:, first_name:, second_name:, third_name:})
+    attributes.each do |key, value|
+      send("#{key}=", value) if respond_to?("#{key}=") && valid_field?(key, value)
+    end
   end
 
-  def id=(id)
-    @id = id.to_s =~ /^\d+$/ ? id : nil
+  def id=(value)
+    @id = value if valid_field?(id, value)
   end
 
-  def git=(git)
-    @git = git.to_s =~ /^[a-zA-Z0-9_.+-]+$/ ? git : nil
+  def first_name=(value)
+    @first_name = value if valid_field?(first_name, value)
   end
 
-  def first_name=(first_name)
-    @first_name = first_name.to_s =~ /^[A-Za-zА-Яа-яЁё'-]+$/ ? first_name : nil
+  def second_name=(value)
+    @second_name = value if valid_field?(second_name, value)
   end
 
-  def second_name=(second_name)
-    @second_name = second_name.to_s =~ /^[A-Za-zА-Яа-яЁё'-]+$/ ? second_name : nil
+  def third_name=(value)
+    @third_name = value if valid_field?(third_name, value)
   end
 
-  def last_name=(last_name)
-    @last_name = last_name.to_s =~ /^[A-Za-zА-Яа-яЁё'-]+$/ ? last_name : nil
+    def self.is_valid_first_name?(value)
+    /\A[A-ZА-я][a-zа-я]*\z/ === value
+  end
+
+  def self.is_valid_middle_name?(value)
+    /\A[A-ZА-я][a-zа-я]*\z/ === value
+  end
+
+  def self.is_valid_last_name?(value)
+    /\A[A-ZА-я][a-zа-я]*\z/ === value
+  end
+
+  def self.is_valid_id(value)
+    value.is_a?(Integer)
+  end
+
+    def valid_field?(field, value)
+    case field
+    when :id
+      self.class.is_valid_id(value)
+    when :first_name
+      self.class.is_valid_first_name?(value)
+    when :second_name
+      self.class.is_valid_middle_name?(value)
+    when :third_name
+      self.class.is_valid_last_name?(value)
+    else
+      true
+    end
   end
 end
