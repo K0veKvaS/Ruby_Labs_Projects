83530f7523c1d8920468dcaac379d28013f3be75
K0veKvaS <vovanco03@mail.ru>
2025-02-06 21:48:42 +0300

    Lab 2.14 expanded short & added fixed private

--- Core classes and models/student_short.rb
+++ Core classes and models/student_short.rb
@@ -1,32 +1,30 @@
-require_relative 'student'
+require_relative 'Class_Student'
 
-class StudentShort < Student
-  attr_reader :git, :id, :initials, :contact
+class Student_short < Student
+  attr_reader :name_and_initials, :contact
 
-  def initialize(student: nil, id: nil, info_string: nil)
-    if student.is_a?(Student)
-      super(second_name: student.second_name, first_name: student.first_name, last_name: student.last_name,
-            telephone: student.telephone, telegram: student.telegram, email: student.email, git: student.git)
-      @git = student.git
-      @id = student.id
-      @initials = student.get_initials 
-      @contact = student.contact_method 
-    elsif info_string
-      parse_info_string(info_string)
-      @id = id
-      @contact = "Контактная информация недоступна" 
-    end
+  def initialize(args)
+    @id = args[:id]
+    @name_and_initials = args[:name_and_initials] if valid_field?(name_and_initials, args[:name_and_initials])
+    @contact = args[:contact] if valid_field?(email, args[:contact]) || valid_field?(telegram, args[:contact]) || valid_field?(telephone, args[:contact])
+    @git = args[:git] if valid_field?(git, args[:git])
   end
 
-  # Метод для парсинга строки информации
-  def parse_info_string(info_string)
-    parts = info_string.split(' ')
-    self.second_name = parts[0]
-    self.first_name = parts[1]
-    self.last_name = parts[2] if parts.length > 2
+  def self.initialize_from_student(student)
+    id = student.id
+    name_and_initials = student.full_name_with_initials
+    contact = student.contact_info
+    git = student.git
+    new(id: id, name_and_initials: name_and_initials, contact: contact, git: git)
   end
-
-  def getInfo
-    "ID: #{@id}, Фамилия и инициалы: #{initials}, Git: #{@git}, контакты: #{@contact}"
+  
+  def self.initialize_from_string(id, string)
+    data = string.split('; ')
+    new(id: id, name_and_initials: data[0], contact: data[1], git: data[2])
   end
+  
+  def validate?
+    !@contact.nil? || has_git?
+  end
+  private_class_method :new
 end
