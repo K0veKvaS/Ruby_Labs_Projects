04f9b940d8561f9a5f2fa8cc1fc2440e6afc62d8
K0veKvaS <vovanco03@mail.ru>
2025-02-06 21:45:30 +0300

    Lab 2.17 expanded student, corrected private

--- Core classes and models/class_student.rb
+++ Core classes and models/class_student.rb
@@ -1,52 +1,83 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :telegram, :telephone, :email
+  attr_reader :telegram, :telephone, :email, :git
 
-# Конструктор для инициализации переменной студента, принимающая хэш определенных переменных, из которых обязательны составляющие: id, фамилия и имя
-  def initialize(id: nil, second_name:, first_name:, last_name: nil, telephone: nil, telegram: nil, email: nil, git: nil)
-    #Принцип наследования: вызов одноименного метода из родительского класса для переданных атрибутов
-    super(second_name: second_name, first_name: first_name, last_name: last_name, id: id, git: git)
-    set_contacts(telephone: telephone, telegram: telegram, email: email)
+  def set_contacts(contacts = {})
+    contacts.each do |key, value|
+      instance_variable_set("@#{key}", value) if [:telephone, :telegram, :email, :git].include?(key.to_sym) && valid_field?(key, value)
+    end
   end
 
-  def set_contacts(telephone: nil, telegram: nil, email: nil)
-    instance_variable_set("@telephone", telephone)
-    instance_variable_set("@email", email)
-    instance_variable_set("@telegram",telegram)
-    protected_methods
-  end
-
-  def contact_method
-    return "Телефон: #{@telephone}" if @telephone
-    return "Telegram: #{@telegram}" if @telegram
-    return "Email: #{@email}" if @email
-    "Контактная информация недоступна"
-  end
-
-  def getInfo
-    contact_info = contact_method
-    "Имя: #{first_name}, Фамилия: #{second_name}, Отчество: #{last_name} (Git: #{@git}, Связь - #{contact_info})"
-  end
-
-  def initials
-    "#{@second_name} #{initials_string}"
-  end
-
-  def initials_string
-    "#{@first_name[0]}.#{@last_name ? " #{@last_name[0]}" : ''}."
+  def info
+    "ID: #{@id}\nФИО: #{@first_name} #{@second_name} #{@third_name}\nКонтактная информация:\n\tТелефон: #{@telephone}\n\tTelegram: #{@telegram}\n\tEmail: #{@email}\nГит:\n\tGit: #{@git}"
   end
   
-    private 
-  def telephone=(telephone)
-    @telephone = telephone.to_s =~ /^\+\d{11}$/
+  def getInfo
+    "#{full_name_with_initials}; #{contact_info}; #{@git}"
   end
 
-  def email=(email)
-    @email = email.to_s =~ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/
+  def has_contact_info?
+    !@telephone.nil? || !@telegram.nil? || !@email.nil?
+  end
+    
+  def has_git?
+    !@git.nil?
+  end
+    
+  def validate?
+    has_contact_info? && has_git?
   end
 
-  def telegram=(telegram)
-    @telegram = telegram.to_s =~ /^@[a-zA-Z0-9_]{5,32}$/
+  def contact_info
+    info = "Телефон: #{@telephone}" if !@telephone.nil?
+    info = "Telegram: #{@telegram}" if !@telegram.nil?
+    info = "Email: #{@email}" if !@email.nil?
+  end
+
+  def full_name_with_initials
+    "#{@third_name} #{first_initial}.#{middle_initial}."
+  end
+
+  def valid_field?(field, value)
+    super(field, value)
+    case field
+    when :telephone
+      self.class.is_valid_number?(value)
+    when :telegram
+      self.class.is_valid_username?(value)
+    when :email
+      self.class.is_valid_email?(value)
+    when :git
+      self.class.is_valid_url?(value)
+    else
+      true
+    end
+  end
+  
+    private #=--------------------------------------------------------------------------
+  
+  def first_initial
+    @first_name.chr
+  end
+
+  def middle_initial
+    @second_name.chr
+  end
+
+  def self.is_valid_number?(value)
+    /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/ === value
+  end
+
+  def self.is_valid_username?(value)
+    /^[a-zA-Z0-9_]+$/ === value
+  end
+
+  def self.is_valid_email?(value)
+    /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]{2,})\z/ === value
+  end
+
+  def self.is_valid_url?(value)
+    /^(https?:\/\/)?((github|bitbucket)\.com)(:[^\/\s]+)?(\/[^\s]+)$/ === value
   end
 end
